fb70fa80b830de0526507d1f17fd6940edb6663a
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-02-29 21:47:57 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,28 +9,30 @@ const App = () => {
   const contacts = useSelector(getContacts);
 
   return (
-    <div className="bg-homeBg bg-cover bg-center bg-fixed h-screen flex flex-col justify-center p-40">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <BiSolidContact className="mx-auto" size="75" color="#4f46e5" />
-        <h1 className="mt-10 text-center text-3xl font-bold leading-9 tracking-tight text-gray-900">
-          Phonebook
-        </h1>
-      </div>
-      <ContactForm />
-      {contacts.length > 0 ? (
-        <>
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Contacts
-          </h2>
+    <div className="h-screen bg-homeBg bg-cover bg-center bg-fixed p-5">
+      <div className="flex flex-col justify-center">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <BiSolidContact className="mx-auto" size="75" color="#4f46e5" />
+          <h1 className="mt-10 text-center text-3xl font-bold leading-9 tracking-tight text-gray-900">
+            Phonebook
+          </h1>
+        </div>
+        <ContactForm />
+        {contacts.length > 0 ? (
+          <>
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+              Contacts
+            </h2>
 
-          <Filter />
-        </>
-      ) : (
-        <p className="mt-10 text-center text-1xl font-bold leading-9 tracking-tight text-gray-600">
-          Your phonebook is empty. Add first contact!
-        </p>
-      )}
-      {contacts.length > 0 && <ContactList />}
+            <Filter />
+          </>
+        ) : (
+          <p className="mt-10 text-center text-1xl font-bold leading-9 tracking-tight text-gray-600">
+            Your phonebook is empty. Add first contact!
+          </p>
+        )}
+        {contacts.length > 0 && <ContactList />}
+      </div>
     </div>
   );
 };
